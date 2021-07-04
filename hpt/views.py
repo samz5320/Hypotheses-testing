1bac4344f2d3dd246cb5fb096ae72babb185f734
SanjayAradhyamath <sanjayam5320@gmail.com>
2021-07-04 19:50:57 +0530

    added forms

--- hpt/views.py
+++ hpt/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse  
 # Create your views here.
 def index(request):
-    return render(request,"index.html") 
+    return render(request,"index.html")
+
+
+def blue(request):
+     request.method=="POST"
+
+def red(request):
+     request.method=="POST"
