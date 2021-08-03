c0391a0b4aa3d267d54be94d719350c697516f26
SanjayAradhyamath <sanjayam5320@gmail.com>
2021-08-04 00:40:18 +0530

    added mean

--- hpt/mean.py
+++ hpt/mean.py
@@ -11,11 +11,34 @@ class Mean:
         self.red_total = self.db['hpt_red'].count_documents(filter={})
         self.blue_total = self.db['hpt_blue'].count_documents(filter={})
 
+        self.docs_total=self.red_total+self.blue_total
+        #self.doc_total=self.red_total+self.blue_total
         self.red_time = 0
         self.blue_time = 0
 
+        self.x = self.db["hpt_red"].find({},{'_id': 0, 'id': 1, 
+                 'count': 1, 'time': 1})
+  
+        print(self.x)
         for x in self.red_docs:
             self.red_time += x['time']
 
         for x in self.blue_docs:
             self.blue_time += x['time']
+        
+        # self.ind_time=[]
+        # for x in (self.red_docs,self.blue_docs):
+        #     print(x["time"])
+        #     for i in x["time"]:
+        #         self.ind_time.append(i)
+        # print(self.ind_time)
+
+        self.mean1=(self.blue_time+self.red_time)/self.docs_total
+        #print(self.mean1)
+        #print(self.doc_total)
+        #print(self.blue_time+self.red_time)
+        #self.total_docs=int(self.red_docs)+self.blue_docs
+
+        #print( str(total_docs))
+m=Mean()
+print(m)
